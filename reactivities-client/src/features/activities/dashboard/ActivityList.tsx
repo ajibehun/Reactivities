578bb6ff4c14d83899ff46da2ad71f010bf20750
ajibehun <ajibfarhan96@gmail.com>
2021-03-22 15:04:32 +0800

    end of section 7

--- reactivities-client/src/features/activities/dashboard/ActivityList.tsx
+++ reactivities-client/src/features/activities/dashboard/ActivityList.tsx
@@ -1,21 +1,12 @@
+import { observer } from "mobx-react-lite";
 import React, { SyntheticEvent, useState } from "react";
 import { Button, Item, Label, Segment } from "semantic-ui-react";
-import { Activity } from "../../../app/models/activity";
 import { useStore } from "../../../app/stores/store";
 
-interface Props {
-  activities: Activity[];
-  deleteActivity: (id: string) => void;
-  submitting: boolean;
-}
-
-export default function ActivityList({
-  activities,
-  deleteActivity,
-  submitting,
-}: Props) {
+export default observer(function ActivityList() {
   const [target, setTarget] = useState("");
   const { activityStore } = useStore();
+  const { deleteActivity, activitiesByDate, loading } = activityStore;
 
   const handleActivityDelete = (
     e: SyntheticEvent<HTMLButtonElement>,
@@ -29,7 +20,7 @@ export default function ActivityList({
     <>
       <Segment>
         <Item.Group divided>
-          {activities.map((activity) => (
+          {activitiesByDate.map((activity) => (
             <Item key={activity.id}>
               <Item.Content>
                 <Item.Header as="a">{activity.title}</Item.Header>
@@ -50,7 +41,7 @@ export default function ActivityList({
                 />
                 <Button
                   name={activity.id}
-                  loading={submitting && target === activity.id}
+                  loading={loading && target === activity.id}
                   onClick={(e) => handleActivityDelete(e, activity.id)}
                   floated="right"
                   content="Delete"
@@ -64,4 +55,4 @@ export default function ActivityList({
       </Segment>
     </>
   );
-}
+});
