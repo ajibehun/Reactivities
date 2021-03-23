c167bf5818fd8b6fa6daac4e1367f0fc656786c6
ajibehun <ajibfarhan96@gmail.com>
2021-03-23 16:38:31 +0800

    add fluentValidation to validate create and edit method in the backend side

--- API/Startup.cs
+++ API/Startup.cs
@@ -6,6 +6,7 @@ using API.Extensions;
 using Application.Activities;
 using Application.Core;
 using AutoMapper;
+using FluentValidation.AspNetCore;
 using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -36,7 +37,10 @@ namespace API
         public void ConfigureServices(IServiceCollection services)
         {
 
-            services.AddControllers();
+            services.AddControllers().AddFluentValidation(config =>
+            {
+                config.RegisterValidatorsFromAssemblyContaining<Create>();
+            });
             services.AddApplicationServices(_config);
         }
 
